eccb95c7c773f3c8c71bd1e328a7d65bd9ff31ae
mauri <mauricio.macedo.villarnobo@gmail.com>
2025-07-15 02:22:50 -0300

    deploy-update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b9a@03k_yvfyt#=4&3l5-)tgkg&(5qashm+^jf49x_@7*m%v=9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iTzMMV.pythonanywhere.com']
 
 
 # Application definition
